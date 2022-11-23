0ef6ac3ed15ae5c5a976cd6724b1b8cf5aec093a
salarjz123 <115222311+salarjz123@users.noreply.github.com>
2022-11-23 15:16:34 +0500

    Added splash screen
    I have made a splash screen of my application. It contains the logo and the name of the application

--- Dictionary-FinalApp/app/src/main/java/com/app/dictionaryfinalapp/MainActivity.java
+++ Dictionary-FinalApp/app/src/main/java/com/app/dictionaryfinalapp/MainActivity.java
@@ -2,13 +2,38 @@ package com.app.dictionaryfinalapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public Button button;
+    public Button button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),SplashActivity.class);
+                startActivity(i);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Homepage.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
