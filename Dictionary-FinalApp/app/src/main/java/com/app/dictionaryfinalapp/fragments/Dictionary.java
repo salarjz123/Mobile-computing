098f981b591d5d7dc389daa68f565f1a33d96443
salarjz123 <115222311+salarjz123@users.noreply.github.com>
2022-12-15 15:27:24 +0500

    Search bars added
    Search bar has been added in the Dictionary and Thesaurus pages.

--- Dictionary-FinalApp/app/src/main/java/com/app/dictionaryfinalapp/fragments/Dictionary.java
+++ Dictionary-FinalApp/app/src/main/java/com/app/dictionaryfinalapp/fragments/Dictionary.java
@@ -2,8 +2,10 @@ package com.app.dictionaryfinalapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,6 +27,7 @@ public class Dictionary extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    SearchView searchView;
 
     public Dictionary() {
         // Required empty public constructor
@@ -54,13 +57,17 @@ public class Dictionary extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
+            };
+
         }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dictionary, container, false);
+        final View rootView = inflater.inflate(R.layout.fragment_dictionary, container, false);
+        searchView = (SearchView) rootView.findViewById(R.id.searchView);
+        return rootView;
     }
 }
